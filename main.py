fbae077d077be4ace981bbc9d12571f81e17f687
emms368 <emily2008portillo@gmail.com>
2025-02-04 16:06:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import random
- longitud_contrasena = int(input("Introduce la longitud de la contraseña: "))
-def gen_pass(longitud_contrasena):
-   caracteres = "+-/*!&$#?=@abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-   contraseña_generada = ""
-   for i in range(longitud_contrasena):
-    contraseña_generada += random.choice(caracteres)
-print("Tu contraseña es", contraseña_generada)
 
-    return password
+longitud_contrasena = int(input("Introduce la longitud de la contraseña: "))
+
+def gen_pass(longitud_contrasena):
+    caracteres = "+-/*!&$#?=@abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    contraseña_generada = ""
+    
+    for i in range(longitud_contrasena):
+        contraseña_generada += random.choice(caracteres)
+    
+    return contraseña_generada
+
+# Llamamos a la función y mostramos la contraseña generada
+print("Tu contraseña es:", gen_pass(longitud_contrasena))
